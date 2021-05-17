34b9996333e10f66dbb339a7dd270a1456b2b898
rymotion <expressservices.developer@gmail.com>
2021-05-16 23:03:33 -0700

    modified Procfile for Heroku
    modified procfile for heroku execution

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fa5121eee3837473e0ade352f1cf9dcd354e1ad1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fa5121eee3837473e0ade352f1cf9dcd354e1ad1',
+    ),
+  ),
+);
